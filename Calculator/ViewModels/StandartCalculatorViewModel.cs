a6e59d3d6dc24235f53ffb125241c96f1873ee57
AlexeyChernikov <alex0899@yandex.ru>
2020-03-07 19:20:41 +0300

    Added ViewModel blanks for StandartCalc and Window

---
+++ Calculator/ViewModels/StandartCalculatorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.ViewModels.Base;
+
+namespace Calculator.ViewModels
+{
+    class StandartCalculatorViewModel : BaseViewModel
+    {
+
+    }
+}
