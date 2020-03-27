e70172363277390fdcbaabc25e97f0eede7dd26a
AlexeyChernikov <alex0899@yandex.ru>
2020-03-27 20:11:57 +0300

    Added size limit for current number

--- src/Calculator/Models/FormingAnExpression/NumberPadLogic.cs
+++ src/Calculator/Models/FormingAnExpression/NumberPadLogic.cs
@@ -7,44 +7,81 @@ using System.Windows;
 
 namespace Calculator.Models.FormingAnExpression
 {
+    /// <summary>
+    /// Logic for forming the current number
+    /// </summary>
     static class NumberPadLogic
     {
+        #region Private members
+
         /// <summary>
-        /// 
+        /// The maximum size of the current number
         /// </summary>
-        /// <returns>
-        /// 
-        /// </returns>
+        private static int currentNumberMaxSize = 16;
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// To add the selected digit to the current number
+        /// </summary>
         public static string SetNumber(string currentNumber, char pressedNumber, out bool whichBtnIsPressed)
         {
             whichBtnIsPressed = true;
-            return currentNumber != "0" ? (currentNumber + pressedNumber) : pressedNumber.ToString();
+
+            if (CurrentNumberSizeCheck(currentNumber))
+            {
+                return currentNumber != "0" ? (currentNumber + pressedNumber) : pressedNumber.ToString();
+            }
+
+            return currentNumber;
         }
 
         /// <summary>
-        /// 
+        /// To add or remove a minus in the current number
         /// </summary>
-        /// <remarks>
-        /// 
-        /// </remarks>
-        /// <returns>
-        /// 
-        /// </returns>
         public static string InvertNumber(string currentNumber)
         {
             return currentNumber != "0" ? (currentNumber.IndexOf('-') == -1 ? currentNumber.Insert(0, "-") : currentNumber.Remove(0, 1)) : currentNumber;
         }
 
         /// <summary>
-        /// 
+        /// To add a comma to the current number
         /// </summary>
-        /// <returns>
-        /// 
-        /// </returns>
         public static string PutAComma(string currentNumber, out bool whichBtnIsPressed)
         {
             whichBtnIsPressed = true;
+
             return currentNumber.IndexOf(',') == -1 ? currentNumber + ',' : currentNumber;
         }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// To check the size of the current number
+        /// </summary>
+        private static bool CurrentNumberSizeCheck(string currentNumber)
+        {
+            int extraSize = 0;
+
+            //If there is a '-' sign in the number
+            if (currentNumber.IndexOf('-') == 0)
+            {
+                extraSize++;
+            }
+
+            //If the number is ','
+            if (currentNumber.IndexOf(',') != -1)
+            {
+                extraSize++;
+            }
+
+            return (currentNumber.Length - extraSize) < currentNumberMaxSize ? true : false;
+        }
+
+        #endregion
     }
 }
