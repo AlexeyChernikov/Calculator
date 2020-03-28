1deedb7ceadf9ed806d7510c95bd30a3b7a69545
AlexeyChernikov <alex0899@yandex.ru>
2020-03-28 18:21:50 +0300

    Update FormingAnExpression.cs
    Added method for converting real numbers

--- src/Calculator/Models/ComposingAnExpression/FormingAnExpression.cs
+++ src/Calculator/Models/ComposingAnExpression/FormingAnExpression.cs
@@ -18,10 +18,17 @@ namespace Calculator.Models.ComposingAnExpression
         /// </summary>
         public static string SetOperation(string currentNumber, string currentExpression, string selectedOperation, ref bool whichBtnIsPressed)
         {
+            bool _whichBtnIsPressed = whichBtnIsPressed;
+
             whichBtnIsPressed = false;
 
-            if (whichBtnIsPressed)
+            if (_whichBtnIsPressed)
             {
+                if (currentNumber.IndexOf(',') != -1 && (currentNumber[currentNumber.Length-1]=='0' || currentNumber[currentNumber.Length - 1] == ','))
+                {
+                    currentNumber = ConvertingRealNumbers(currentNumber);
+                }
+
                 return NegateCheck(currentNumber) ? (currentExpression + '(' + currentNumber + ')' + selectedOperation) : (currentExpression + currentNumber + selectedOperation);
             }
 
@@ -31,6 +38,29 @@ namespace Calculator.Models.ComposingAnExpression
         #endregion
 
         #region Private methods
+
+        /// <summary>
+        /// To convert real numbers
+        /// </summary>
+        /// <example>
+        /// 16,000000 => 16
+        /// 3, => 3
+        /// 3,1020000 => 3,102
+        /// </example>
+        /// <returns>
+        /// The edited string of the current number
+        /// </returns>
+        private static string ConvertingRealNumbers(string currentNumber)
+        {
+            StringBuilder stringBuilderCurNo = new StringBuilder(currentNumber);
+
+            while (stringBuilderCurNo.Length > 1 && (stringBuilderCurNo[stringBuilderCurNo.Length-1] == '0' || stringBuilderCurNo[stringBuilderCurNo.Length - 1] == ','))
+            {
+                stringBuilderCurNo.Remove(stringBuilderCurNo.Length-1, 1);
+            }
+
+            return stringBuilderCurNo.ToString();
+        }
 
         /// <summary>
         /// To check if a current number is negative
