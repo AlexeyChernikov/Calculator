c69731dfd955c23a70b6865f888ef178bfeb78fd
AlexeyChernikov <alex0899@yandex.ru>
2020-04-09 22:38:55 +0300

    Issue #18
    Close #18

--- src/Calculator/Models/MakeAnExpression/NumberStandardization.cs
+++ src/Calculator/Models/MakeAnExpression/NumberStandardization.cs
@@ -1,6 +1,4 @@
-﻿using System.Text;
-
-namespace Calculator.Models.MakeAnExpression
+﻿namespace Calculator.Models.MakeAnExpression
 {
     /// <summary>
     /// Contains methods that bring the passed number to a general form
@@ -17,53 +15,14 @@ namespace Calculator.Models.MakeAnExpression
         /// </returns>
         public static string Standardization(string number)
         {
-            if (number.IndexOf(',') != -1 && (number[number.Length - 1] == '0' || number[number.Length - 1] == ','))
-            {
-                number = ConvertingRealNumbers(number);
-            }
+            number = double.Parse(number).ToString();
 
-            if (number[0] == '-')
+            if (number.IndexOf('-') != -1)
             {
-                number = NegatePutInParentheses(number);
+                number = '(' + number + ')';
             }
 
             return number;
-        }
-
-        #endregion
-
-        #region Private methods
-
-        //смотри issue #18
-        /// <summary>
-        /// To convert real number
-        /// </summary>
-        /// <example>
-        /// 16,000000 => 16
-        /// 3, => 3
-        /// 3,1020000 => 3,102
-        /// </example>
-        /// <returns>
-        /// The edited string of the current number
-        /// </returns>
-        private static string ConvertingRealNumbers(string number)
-        {
-            StringBuilder stringBuilderCurNo = new StringBuilder(number);
-
-            while (stringBuilderCurNo.Length > 1 && (stringBuilderCurNo[stringBuilderCurNo.Length - 1] == '0' || stringBuilderCurNo[stringBuilderCurNo.Length - 1] == ','))
-            {
-                stringBuilderCurNo.Remove(stringBuilderCurNo.Length - 1, 1);
-            }
-
-            return stringBuilderCurNo.ToString();
-        }
-
-        /// <summary>
-        /// To put a negative number in parentheses
-        /// </summary>
-        private static string NegatePutInParentheses(string number)
-        {
-            return '(' + number + ')';
         }
 
         #endregion
