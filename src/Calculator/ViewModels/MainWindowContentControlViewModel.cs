916b82afc7bcbb91334af733d6bb1546788570fd
AlexeyChernikov <alex0899@yandex.ru>
2020-04-23 16:34:39 +0300

    Added stubs for unrealized functions

---
+++ src/Calculator/ViewModels/MainWindowContentControlViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Calculator.ViewModels.Base;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Calculator.ViewModels
+{
+    class MainWindowContentControlViewModel : BaseViewModel
+    {
+        #region Commands
+
+        /// <summary>
+        /// Nope!!!
+        /// </summary>
+        public ICommand NopeCommand { get; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MainWindowContentControlViewModel()
+        {
+            NopeCommand = new RelayCommand(() => MessageBox.Show("Nope!!!"));
+        }
+
+        #endregion
+    }
+}
